6f748e47e20a5b51d93ec51eff4ab977d15be5ec
HaythamFourati <shadowfourati@gmail.com>
2024-11-16 09:34:56 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '46e63a8b-f427-4d5b-a1c1-aa8d7e442d55';
+export const MODAL_CLOSE_SEC = 2.5;
